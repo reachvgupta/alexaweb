9f6e77611bb7bfd479a557eb260bc5bdb4240655
Vaibhav Gupta <vaibgupt@amazon.com>
2018-12-18 16:05:42 +0000

    updated

--- creds.py
+++ creds.py
@@ -1,8 +1,8 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
+Security_Profile_Description ="alexa-web1.herokuapp.com"
+Security_Profile_ID  = "alexa-web1.herokuapp.com"
+Client_ID = "amzn1.application-oa2-client.98fad35cf0ed472385a56405a4cf6bcf"
+Client_Secret = "f24ff3552af89188d48437fa5866b52634f4da06bbf36ba8fd8a5e307966ab86"
 Product_ID = ""
 
 #Redis
